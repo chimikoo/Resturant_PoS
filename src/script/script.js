5852ab6fd2d296ef9cc410d8f9d62a3aa5db0b6a
Per-Emil Johansson <chimikoo@gmail.com>
2023-11-28 11:26:14 +0100

    added more clarity for items.

--- src/script/script.js
+++ src/script/script.js
@@ -79,7 +79,15 @@ fs.readFile(menuFilePath, "utf8", (_err, data) => {
       console.log(`\nCategory: ${selectedCategory}`);
       console.log("Items:");
       categorizedMenu[selectedCategory].forEach((item, index) => {
-        console.log(`${index + 1}. ${item.name}`);
+        console.log(`
+        |------------------------------|
+        | ${index + 1}. ${item.name}\n                          
+        | Price: €${item.price}                                 
+        | Vegan: ${item.isVegan ? "Yes" : "No"}           
+        | Vegetarian: ${item.isVegetarian ? "Yes" : "No"} 
+        |------------------------------|
+        `);
+        
       });
 
       rl.question(
